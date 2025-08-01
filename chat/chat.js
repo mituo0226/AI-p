a2ded23ed04ecd537ebce28691bb25220bf25588
mituo0226 <mituo0226@gmail.com>
2025-08-01 10:59:01 +0900

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -57,7 +57,7 @@ function removeTypingIndicator() {
 function showSystemMessage() {
   const log = document.getElementById('chat-log');
   const msg = document.createElement('div');
-  msg.className = 'message system';
+  msg.className = 'message asakura system';
   msg.innerHTML = `
     <strong>◆ お知らせ ◆</strong><br>
     ここから先の会話は <strong>チケット</strong> が必要です。<br>
