f9c64322f58d6653ece49f566f63d63429a4bc43
mituo0226 <mituo0226@gmail.com>
2025-08-01 18:07:53 +0900

    Delete viewport-fix.js

--- chat/viewport-fix.js
+++
@@ -1,8 +0,0 @@
-
-// JS for setting --vh to fix iOS Chrome 100vh bugs
-function updateViewportHeight() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-window.addEventListener('resize', updateViewportHeight);
-window.addEventListener('load', updateViewportHeight);
